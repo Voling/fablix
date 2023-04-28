799323cc92d56f4b4704e49ea5658fadb38f7ef4
Dean <yiqund4.com>
2023-04-28 00:47:53 -0700

    almost fixing frontend

--- movie_rest_api/WebContent/index.js
+++ movie_rest_api/WebContent/index.js
@@ -8,137 +8,161 @@
  *      2. Populate the data to correct html elements.
  */
 
-
 /**
  * Handles the data returned by the API, read the jsonObject and populate data into html elements
  * @param resultData jsonObject
  */
 
-
-
-
-function handle(resultData){
-
-    handleMovieResult(transformdata(resultData));
-
+function handle(resultData) {
+  handleMovieResult(transformdata(resultData));
 }
 
-function transformdata(resultdata){
-    let movie = ""
-    let prevstar = ""
-    let prevgenre = ""
-    let array = []
-    let length = -1
-    for (let i = 0; i < resultdata.length; i++){
-        if(resultdata[i].id != movie){
-            array.push({ title: resultdata[i].title,id:resultdata[i].id ,year:resultdata[i].year, director:resultdata[i].director,rating:resultdata[i].rating,genres:resultdata[i].genre,cast:[[resultdata[i].star,resultdata[i].starid]]})
-            movie = resultdata[i].id
-            prevgenre = resultdata[i].genre;
-            prevstar = resultdata[i].star;
-            length += 1;
-        }
-        else{
-            if(resultdata[i].genre != prevgenre){
-                array[length].genres += " " + resultdata[i].genre
-                prevgenre = resultdata[i].genre
-            }
-            if(resultdata[i].star != prevstar){
-                array[length].cast.push([resultdata[i].star,resultdata[i].starid])
-                prevstar = resultdata[i].star
-                console.log(resultdata[i].starid)
-            }
-        }
+function transformdata(resultdata) {
+  let movie = "";
+  let prevstar = "";
+  let prevgenre = "";
+  let array = [];
+  let length = -1;
+  for (let i = 0; i < resultdata.length; i++) {
+    if (resultdata[i].id != movie) {
+      array.push({
+        title: resultdata[i].title,
+        id: resultdata[i].id,
+        year: resultdata[i].year,
+        director: resultdata[i].director,
+        rating: resultdata[i].rating,
+        genres: resultdata[i].genre,
+        cast: [[resultdata[i].star, resultdata[i].starid]],
+      });
+      movie = resultdata[i].id;
+      prevgenre = resultdata[i].genre;
+      prevstar = resultdata[i].star;
+      length += 1;
+    } else {
+      if (resultdata[i].genre != prevgenre) {
+        array[length].genres += " " + resultdata[i].genre;
+        prevgenre = resultdata[i].genre;
+      }
+      if (resultdata[i].star != prevstar) {
+        array[length].cast.push([resultdata[i].star, resultdata[i].starid]);
+        prevstar = resultdata[i].star;
+        console.log(resultdata[i].starid);
+      }
     }
-    console.log(array)
-    return array
-
+  }
+  console.log(array);
+  return array;
 }
 function handleMovieResult(resultData) {
-    console.log("handleStarResult: populating star table from resultData");
+  console.log("handleStarResult: populating star table from resultData");
 
-    // Populate the star table
-    // Find the empty table body by id "star_table_body"
-    let starTableBodyElement = jQuery("#movie_table_body");
-    console.log(resultData)
-    // Iterate through resultData, no more than 10 entries
-    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+  // Populate the star table
+  // Find the empty table body by id "star_table_body"
+  let starTableBodyElement = jQuery("#movie_table_body");
+  console.log(resultData);
+  // Iterate through resultData, no more than 10 entries
+  for (let i = 0; i < Math.min(20, resultData.length); i++) {
+    // Concatenate the html tags with resultData jsonObject
+    let rowHTML = "";
+    rowHTML += "<tr>";
+    rowHTML +=
+      '<th class="rounded-th">' +
+      // Add a link to single-star.html with id passed with GET url parameter
+      '<a href="single-movie.html?id=' +
+      resultData[i]["id"] +
+      '">' +
+      resultData[i]["title"] + // display star_name for the link text
+      "</a>" +
+      "</th>";
+    rowHTML += '<th class="rounded-th">' + resultData[i]["year"] + "</th>";
+    rowHTML += '<th class="rounded-th">' + resultData[i]["director"] + "</th>";
+    rowHTML += '<th class="rounded-th">' + resultData[i]["genres"] + "</th>";
+    rowHTML +=
+      '<th class="rounded-th">' +
+      '<a href="single-star.html?id=' +
+      resultData[i]["cast"][0][1] +
+      '">' +
+      resultData[i]["cast"][0][0] + // display star_name for the link text
+      "</a>" +
+      " " +
+      '<a href="single-star.html?id=' +
+      resultData[i]["cast"][1][1] +
+      '">' +
+      resultData[i]["cast"][1][0] + // display star_name for the link text
+      "</a>" +
+      " " +
+      '<a href="single-star.html?id=' +
+      resultData[i]["cast"][2][1] +
+      '">' +
+      resultData[i]["cast"][2][0] + // display star_name for the link text
+      "</a>" +
+      "</th>";
+    rowHTML += '<th class="rounded-th">' + resultData[i]["rating"] + "</th>";
+    rowHTML += "</tr>";
 
-        // Concatenate the html tags with resultData jsonObject
-        let rowHTML = "";
-        rowHTML += "<tr>";
-        rowHTML +=
-            "<th class=\"rounded-th\">" +
-            // Add a link to single-star.html with id passed with GET url parameter
-            '<a href="single-movie.html?id=' + resultData[i]['id'] + '">'
-            + resultData[i]["title"] +     // display star_name for the link text
-            '</a>' +
-            "</th>";
-        rowHTML += "<th class=\"rounded-th\">" + resultData[i]["year"] + "</th>";
-        rowHTML += "<th class=\"rounded-th\">" + resultData[i]["director"] + "</th>";
-        rowHTML += "<th class=\"rounded-th\">" + resultData[i]["genres"] + "</th>";
-        rowHTML += "<th class=\"rounded-th\">" + '<a href="single-star.html?id=' + resultData[i]["cast"][0][1]+ '">'
-            + resultData[i]["cast"][0][0] +     // display star_name for the link text
-            '</a>'+  " " + '<a href="single-star.html?id=' + resultData[i]["cast"][1][1]+ '">'
-            + resultData[i]["cast"][1][0] +     // display star_name for the link text
-            '</a>' + " " + '<a href="single-star.html?id=' + resultData[i]["cast"][2][1]+ '">'
-            + resultData[i]["cast"][2][0] +    // display star_name for the link text
-            '</a>'+ "</th>";
-        rowHTML += "<th class=\"rounded-th\">" + resultData[i]["rating"] + "</th>";
-        rowHTML += "</tr>";
-
-        // Append the row created to the table body, which will refresh the page
-        starTableBodyElement.append(rowHTML);
-    }
+    // Append the row created to the table body, which will refresh the page
+    starTableBodyElement.append(rowHTML);
+  }
 }
-$(document).ready(function() {
-    //find button and attach logout fx to it
-    $('#logoutButton').click(function(){
-        $.ajax({
-            url: "api/logout",
-            type: "POST",
-            success: function(response) {
-                window.location.replace("login.html")
-            }
-        })
-    })
-    $("#searchSubmit").click(function(event) {
-        event.preventDefault();
-        console.log($("#search"));
-        let formData = $("#search").serializeArray();
-        console.log("form" + formData);
-        let data = {};
-
-        for (let field of formData) {
-            if (field.value === "") {
-
-            }
-            else {
-                if (field.type === "TEXT") {
-                    //data[field.name] = "%" + field.value + "%"; // Wrap the title value with % characters
-                } else {
-                    data[field.name] = field.value;
-                }
-            }
-
-        }
-        console.log("here!hey")
-        $.ajax({
-            url: "/search", // Your server-side script that processes the search
-            type: "GET",
-            data: data,
-            success: (resultData) => handle(resultData)
-        });
+$(document).ready(function () {
+  //find button and attach logout fx to it
+  $("#logoutButton").click(function () {
+    $.ajax({
+      url: "api/logout",
+      type: "POST",
+      success: function (response) {
+        window.location.replace("login.html");
+      },
     });
-
-})
+  });
+  $("#searchSubmit").click(function (event) {
+    event.preventDefault();
+    console.log($("#search"));
+    let formData = $("#search").serializeArray();
+    console.log("form" + formData);
+    let data = "";
+    let counter = 0;
+    for (let field of formData) {
+      //console.log(`type:${field.TYPE}`)
+      if (field.value === "") {
+      } else {
+        if (counter != 0) {
+          data += "&";
+        }
+        if (
+          field.name === "title" ||
+          field.name === "director" ||
+          field.name === "star"
+        ) {
+          console.log("hey soul sister");
+          //data[field.name] = "%" + field.value + "%"; // Wrap the title value with % characters
+          data += field.name + "=%25" + field.value + "%25";
+        } else {
+          data += field.name + "=" + field.value;
+        }
+        counter += 1;
+      }
+    }
+    console.log("here!hey");
+    console.log(data);
+    $.ajax({
+      url: "search?" + data, // Your server-side script that processes the search
+      type: "GET",
+      success: (resultData) => {
+        console.log(resultData);
+        handle(resultData);
+      },
+    });
+  });
+});
 /**
  * Once this .js is loaded, following scripts will be executed by the browser
  */
 
 // Makes the HTTP GET request and registers on success callback function handleStarResult
 jQuery.ajax({
-    dataType: "json", // Setting return data type
-    method: "GET", // Setting request method
-    url: "api/movies", // Setting request url, which is mapped by StarsServlet in Stars.java
-    success: (resultData) => handle(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+  dataType: "json", // Setting return data type
+  method: "GET", // Setting request method
+  url: "api/movies", // Setting request url, which is mapped by StarsServlet in Stars.java
+  success: (resultData) => handle(resultData), // Setting callback function to handle data returned successfully by the StarsServlet
 });
